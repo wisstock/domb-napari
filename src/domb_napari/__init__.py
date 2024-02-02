7149f583c54112f0682c59ea82969ea9b680fd89
wisstock <omnia.fatum@gmail.com>
2024-02-02 16:31:09 +0200

    2.02.2024: ctrl commit

--- src/domb_napari/__init__.py
+++ src/domb_napari/__init__.py
@@ -3,9 +3,13 @@ from ._widget import split_sep
 from ._widget import der_series, mask_calc
 from ._widget import labels_profile_stat, labels_profile_line
 
+from ._reader import get_reader, oib_file_reader
+
 __all__ = ['split_channels',
            'split_sep',
            'der_series',
            'mask_calc',
            'labels_profile_stat',
-           'labels_profile_line']
+           'labels_profile_line',
+           'get_reader',
+           'oib_file_reader']
